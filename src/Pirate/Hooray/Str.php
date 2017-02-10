5835dc95a10faaf9f110365601118cfd51f99233
David Zurborg <david@git.spr.io>
2017-02-10 18:53:22 +0100

    fix documentation

--- src/Pirate/Hooray/Str.php
+++ src/Pirate/Hooray/Str.php
@@ -112,8 +112,8 @@ class Str
      * Str::pluralize('{No|One|$} quer(y|ies) (is|are) found', 1); // 'One query is found'
      * ```
      *
-     * + Rule #1: `(pl)` expands when the amount is exactly one. Otherwise this expression is omitted
-     * + Rule #2: `{sl}` expands when the amount is not one. This is the opposite of rule #1.
+     * + Rule #1: `(pl)` expands when the amount is not one. Otherwise this expression is omitted
+     * + Rule #2: `{sl}` expands when the amount is exactly one. This is the opposite of rule #1.
      * + Rule #3: `(one|two|three|four|all other)` expands to element in the list, whereas 0 expands to the last element.
      * + Rule #4: `{zero|one|two|three|all other}` expands to element in the list, whereas 0 expands to the first element.
      * + Rule #5: `$` expands to the numeric value of amount. This replacement can be changed with the 3rd parameter and defaults to the dollar sign.
